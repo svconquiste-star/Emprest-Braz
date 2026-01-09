9a11c5719b79177648eeb25d362c25c1a35303cb
svconquiste-star <seu_email@github.com>
2026-01-09 17:26:26 -0300

    config: add basePath and assetPrefix for Coolify subpath deployment

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   output: 'standalone',
+  basePath: '/braz-emprestimos',
+  assetPrefix: '/braz-emprestimos',
 }
 
 module.exports = nextConfig
