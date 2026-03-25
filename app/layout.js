a49c38c28153f6de2d366178b6054de02567e159
Emprest-Nunes Developer <dev@emprest-nunes.com>
2026-03-25 14:05:04 -0300

    Fix Pixel detection: use native inline script instead of Next.js Script component for immediate load

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,5 @@
 import './globals.css';
 
-import Script from 'next/script';
 import { Outfit, Merriweather } from 'next/font/google';
 import Providers from './providers';
 
@@ -34,41 +33,31 @@ export default function RootLayout({ children }) {
           crossOrigin="anonymous"
           referrerPolicy="no-referrer"
         />
-        <Script
-          id="persist-fbc"
-          strategy="beforeInteractive"
+        <script
           dangerouslySetInnerHTML={{
             __html: `
-              (function(){
-                try {
-                  var url = new URL(window.location.href);
-                  var fbclid = url.searchParams.get('fbclid');
-                  if (fbclid) {
-                    var fbc = 'fb.1.' + Date.now() + '.' + fbclid;
-                    var expires = new Date(Date.now() + 90 * 24 * 60 * 60 * 1000).toUTCString();
-                    document.cookie = '_fbc=' + fbc + '; path=/; expires=' + expires + '; SameSite=Lax';
-                  }
-                } catch(e){}
-              })();
-            `,
-          }}
-        />
-        <Script
-          id="meta-pixel"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              !function(f,b,e,v,n,t,s){
-                if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-                n.queue=[];t=b.createElement(e);t.async=!0;
-                t.src=v;s=b.getElementsByTagName(e)[0];
-                s.parentNode.insertBefore(t,s)
-              }(window, document,'script','https://connect.facebook.net/en_US/fbevents.js');
-              fbq('set','autoConfig',false,'${PIXEL_ID}');
-              fbq('init','${PIXEL_ID}');
-              fbq('track','PageView');
+(function(){
+  try {
+    var url = new URL(window.location.href);
+    var fbclid = url.searchParams.get('fbclid');
+    if (fbclid) {
+      var fbc = 'fb.1.' + Date.now() + '.' + fbclid;
+      var expires = new Date(Date.now() + 90 * 24 * 60 * 60 * 1000).toUTCString();
+      document.cookie = '_fbc=' + fbc + '; path=/; expires=' + expires + '; SameSite=Lax';
+    }
+  } catch(e){}
+})();
+!function(f,b,e,v,n,t,s){
+  if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+  n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t,s)
+}(window, document,'script','https://connect.facebook.net/en_US/fbevents.js');
+fbq('set','autoConfig',false,'${PIXEL_ID}');
+fbq('init','${PIXEL_ID}');
+fbq('track','PageView');
             `,
           }}
         />
